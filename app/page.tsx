7cb9bd776e64f08143867341edb01a4282963c78
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-06-29 01:34:53 +0530

    added files

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Hero from "@/components/Hero";
+import FloatingNav from "../components/ui/FloatingNav"; // Import the FloatingNav component
+import { FaHome } from 'react-icons/fa'; // Import the FaHome icon
+import Grid from "@/components/Grid";
+
+
+export default function Home() {
+  return (
+   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-7xl w-full">
+      <FloatingNav navItems={[
+        { name: "Home", link: "/", icon: <FaHome />},
+      ]} />
+      <Hero />
+      <Grid />
+    </div>
+   </main>
+    
+  );
+}
